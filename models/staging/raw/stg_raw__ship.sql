37473cfcdbbc42f313045ec28071645e1f8a74f8
Rouxinol Placido <placidorouxinol@gmail.com>
2024-07-18 11:40:21 +0000

    dbt build --select int_sales_margin

---
+++ models/staging/raw/stg_raw__ship.sql
@@ -0,0 +1,17 @@
+with 
+
+source as (
+
+    select * from {{ source('raw', 'ship') }}
+
+),
+
+renamed as (
+
+    select orders_id, shipping_fee, logCost, ship_cost
+
+    from source
+
+)
+
+select * from renamed
